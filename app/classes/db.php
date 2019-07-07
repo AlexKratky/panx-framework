e44a42b47dea5a780db62e9dde7358c685b6e65a
Alex Kratky <info@alexkratky.cz>
2019-07-07 14:49:50 +0200

    Added comments

--- app/classes/db.php
+++ app/classes/db.php
@@ -1,12 +1,37 @@
 <?php
+/**
+ * @name db.php
+ * @link https://alexkratky.cz                          Author website
+ * @link https://panx.eu/docs/                          Documentation
+ * @link https://github.com/AlexKratky/panx-framework/  Github Repository
+ * @author Alex Kratky <info@alexkratky.cz>
+ * @copyright Copyright (c) 2019 Alex Kratky
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @description Class to work with database. Part of panx-framework.
+ */
+
 class db {
+    /**
+     * @var PDO $conn The connection to db.
+     */
 	private static $conn;
 
+    /**
+     * @var array $settings The connection's settings.
+     */
     private static $settings = array(
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
     );
 
+    /**
+     * Create connection to database.
+     * @param string $host The hostname of DB server.
+     * @param string $user The username of DB.
+     * @param string $pass The password of DB user.
+     * @param string $db The database name.
+     * @return PDO The new connection.
+     */
     public static function connect($host, $user, $pass, $db)
     {
         if (!isset(self::$conn))
@@ -21,6 +46,11 @@ class db {
         return self::$conn;
     }
 
+    /**
+     * Execute query on DB.
+     * @param string $sql The query. Use ? for parameters.
+     * @param array $params The array of parameters.
+     */
     public static function query($sql, $params = array())
     {
         $query = self::$conn->prepare($sql);
@@ -28,6 +58,11 @@ class db {
         return $query;
     }
 
+    /**
+     * Execute query on DB and fetch the result.
+     * @param string $sql The query. Use ? for parameters.
+     * @param array $params The array of parameters.
+     */
     public static function select($sql, $params = array()) {
         $q = self::$conn->prepare($sql);
         $q->execute($params);
@@ -35,6 +70,11 @@ class db {
         return $data;
     }
 
+    /**
+     * Execute query on DB and fetch all rows of result.
+     * @param string $sql The query. Use ? for parameters.
+     * @param array $params The array of parameters.
+     */
     public static function multipleSelect($sql, $params = array()) {
         $q = self::$conn->prepare($sql);
         $q->execute($params);
@@ -42,6 +82,11 @@ class db {
         return $data;
     }
 
+    /**
+     * Returns the number provided by COUNT query.
+     * @param string $sql The query. Use ? for parameters.
+     * @param array $params The array of parameters.
+     */
     public static function count($sql, $params = array()) {
         $q = self::$conn->prepare($sql);
         $q->execute($params);
